3951114ee0536d1000fc8e46dd86984f2a98f015
Marcus <marcusyoung0603@gmail.com>
2020-07-10 23:56:10 -0700

    minor css changes

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -4,17 +4,19 @@ const ListItem = (props) => {
 
   return (
     <div className="col mb-4">
-      <div className="card h-100" style={{ width: '14rem' }}>
-        <div style={{ height: '280px', overflow: 'hidden' }}>
-          <img src={props.item.image} className="card-img-top"></img>
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{props.item.name}</h5>
-          <p className="card-text">Fighting vs {props.item.next_opponent} on {props.item.next_fight}</p>
-        </div>
-        <button id={props.item.name} className="btn btn-dark" onClick={(event) => {
-          props.removeHandler(event.target.id)
-        }}>Remove</button>
+      <div className="card h-100 mx-auto" style={{ width: '12rem' }}>
+
+          <div style={{ height: '280px', overflow: 'hidden'}}>
+            <img src={props.item.image} className="card-img-top"></img>
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{props.item.name}</h5>
+            <p className="card-text">Fighting vs {props.item.next_opponent} on {props.item.next_fight}</p>
+          </div>
+          <button id={props.item.name} className="btn btn-dark" onClick={(event) => {
+            props.removeHandler(event.target.id)
+          }}>Remove</button>
+
       </div>
     </div>
 
